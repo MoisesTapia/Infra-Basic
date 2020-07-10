ca1579dca474f40ca4af093db202175dd72a455a
MoisesTapia <moisestapia741@gmail.com>
2020-07-10 15:35:12 -0500

    New feature in this file

--- verify_infra.sh
+++ verify_infra.sh
@@ -3,9 +3,10 @@
 FILE=$PWD
 
 echo -e "\e[1;32mWait a moment .....\e[0m"
+sleep 2
 echo -e "\e[0;31mGenereting the report with Inspec ....\e[0m"
 mkdir report
 inspec exec infrabasic --reporter json:$FILE/report/output.json html:$FILE/report/index.html -t aws://
-echo -e "\e[0;32m|------------------------------------------------------------------------------|\e[0m"
-echo -e "\e[0;32m| File saived in the following path $FILE|\e[0m"
-echo -e "\e[0;32m|------------------------------------------------------------------------------|\e[0m"
+echo -e "\e[0;32m|-----------------------------------------------------------------------------|\e[0m"
+echo -e "\e[0;32m| File saved in the following path $FILE|\e[0m"
+echo -e "\e[0;32m|-----------------------------------------------------------------------------|\e[0m"
