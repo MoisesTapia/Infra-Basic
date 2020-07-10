ba4de9f70d808a4e838a4ecc9b3d7fc587dc9d47
MoisesTapia <moisestapia741@gmail.com>
2020-07-10 12:32:41 -0500

    Add new features to make a folder and inside it save the report fails

--- verify_infra.sh
+++ verify_infra.sh
@@ -2,7 +2,10 @@
 
 FILE=$PWD
 
-echo "Wait a moment ....."
-echo "Genereting the report with Inspec ...."
-inspec exec infrabasic --reporter cli json:$FILE/output.json html:$FILE/index.html -t aws://
-echo "File saived in the following path $FILE"
+echo -e "\e[1;32mWait a moment .....\e[0m"
+echo -e "\e[0;31mGenereting the report with Inspec ....\e[0m"
+mkdir report
+inspec exec infrabasic --reporter json:$FILE/report/output.json html:$FILE/report/index.html -t aws://
+echo -e "\e[0;32m|------------------------------------------------------------------------------|\e[0m"
+echo -e "\e[0;32m| File saived in the following path $FILE|\e[0m"
+echo -e "\e[0;32m|------------------------------------------------------------------------------|\e[0m"
