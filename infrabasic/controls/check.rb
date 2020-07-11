867b3e81c0376e80b30d455f123d2afb66c05fdc
MoisesTapia <moisestapia741@gmail.com>
2020-07-11 12:48:56 -0500

    Add new features to check infra

--- infrabasic/controls/check.rb
+++ infrabasic/controls/check.rb
@@ -1,53 +1,76 @@
-# copyright: 2018, Moises Pablo Ortiz Tapia
-# github https://github.com/MoisesTapia
 require 'json'
-content = File.read("/home/moisestapia/EMS/Terraform/Infra-Basic/infrabasic/files")
+content = File.read("/home/moisestapia/EMS/Terraform/Infra-Basic/infrabasic/files/terraform.json")
 params = JSON.parse(content)
 
-################### Instance EC2 ###################
-# Public IP
-public_ip_addrs = params['aws_security_group_public_ip']['value']
+# copyright: 2018, Moises Tapia
+# github https://github.com/MoisesTapia
 
+################### Instance EC2 
 # images id
 id_image = params['aws_instance']['value']
-
+# AMI Image
+ami_image = params['ec2_instance_ami']['value']
+# Public IP
+public_ip_addrs = params['secgroup_public_ip']['value']
 # availability zone
-azone_var = params['vpc_cidr']['value']
-
+azone_var = params['secgroup_av_zone']['value']
 # Security Group IDS
-secgroup_ids = params['vpc_cidr']['value']
+secgroup_ids = params['security_group_id']['value'] 
 
-title "Infra Basic"
+################### Internal VARS
+ENDPOINT_NAME = attribute('endpointname') #
+STATE_VPC = attribute('vpcstate')         # YOU CAN MODIIFY THIS ATTRIBUTES inspec.yml
+REGION_INFRA = attribute('region')        #
+DEFAULT_VPC = attribute('defaultvpc')     #
+
+title "Test of Infra-Security "
 
 
-control "Intance EC2" do
+control "Testing 01 - Intance EC2" do
   impact 0.5
   title "The instance EC2 should be exist"
   desc ""
 
-  describe aws_ec2_instance('i-0b95f7c75040cff5c') do
-    it  { should_not have_roles }
-    it  { should be_running }
-    its ('image_id') { should eq ['id_image'] }
-    its ('availability_zone') { should eq ['azone_var'] }
-    its('public_ip_address') { should eq ['public_ip_addrs'] }
-    its('security_group_ids') { should include ['secgroup_ids'] }
+  describe aws_ec2_instance(id_image) do
+    it                          { should_not have_roles }
+    it                          { should be_running }
+    its ('image_id')            { should eq ami_image }
+    its ('availability_zone')   { should eq azone_var }
+    its ('public_ip_address')   { should eq public_ip_addrs }
+    its ('security_group_ids')  { should include secgroup_ids }
+    its ('vpc_id')              { should eq DEFAULT_VPC }
 
   end  
 end
 
-control "Region of Infra" do
+control "Testing 02 - VPC Check" do
+  impact 0.5
+  title "Verify VPC"
+  desc "---"
+
+  describe aws_vpc do
+    its ('state') { should eq STATE_VPC }
+  end
+
+  describe aws_vpc(DEFAULT_VPC) do
+    it            { should be_default }
+  end
+  
+
+end
+
+control "Testing 03 - Region of Infra" do
   impact 0.8
   title "Verify de region"
   desc "Verify that ec2 instance are in the correct region with this control"
 
-  describe aws_region('us-east-2') do
-    it { should exist }
-    its('endpoint') { should eq 'ec2.us-east-2.amazonaws.com' }
+  describe aws_region(REGION_INFRA) do
+    it              { should exist }
+    its('endpoint') { should eq ENDPOINT_NAME }
   end
 end
 
-control "Security Group" do
+control "Testing 04 - Security Group" do
   impact 1
   title "Verify the security group exist"
   desc "with this control we can verufy the SesGrp"
