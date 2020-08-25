d3d42d2d2ea8b539e7a8af6a17c55b5853546030
root <root@moisestapia>
2020-08-25 10:39:20 -0500

    Change syntax of new version of terraform 0.12.0

--- outputs.tf
+++ outputs.tf
@@ -2,29 +2,29 @@
 # Instance
 
 output "aws_instance" {
-    value = "${aws_instance.linux2.id}"
+    value = aws_instance.linux2.id
 }
 
 output "ec2_instance_ami" {
-    value = "${aws_instance.linux2.ami}"
+    value = aws_instance.linux2.ami
 }
 # Security Group
 
 output "security_group_id" {
-    value = "${aws_security_group.instance.id}"
+    value = aws_security_group.instance.id
 }
 
 output "secgroup_public_ip" {
-    value = "${aws_instance.linux2.public_ip}"
+    value = aws_instance.linux2.public_ip
 }
 
 output "secgroup_av_zone" {
-    value = "${aws_instance.linux2.availability_zone}"
+    value = aws_instance.linux2.availability_zone
 }
 
 # VPC 
 output "vpc_group" {
-    value = "${aws_instance.linux2.vpc_security_group_ids}"
+    value = aws_instance.linux2.vpc_security_group_ids
 }
 #output "vpc_id" {
 #    value = ${aws_instance.linux2}
